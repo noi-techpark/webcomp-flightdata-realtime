3a99e14e0dcb620949397039b6562c0ef8e7e73c
Simon Dalvai <s.dalvai@noi.bz.it>
2023-12-12 09:48:08 +0100

    reuse: add some licenses and headers

--- src/components/DetailsOverlay.vue
+++ src/components/DetailsOverlay.vue
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
     <div :class="'container-flex noi-details-container '+device+ ' ' + options.theme"> <!-- :class="{hide: !show}" -->
         <div class="row" v-if="isMobile">
